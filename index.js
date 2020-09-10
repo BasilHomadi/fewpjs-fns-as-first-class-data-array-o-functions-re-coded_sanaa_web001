33ecdcc42f99c0c7883cdfdef118d8316da1ac15
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 10:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${DogBreed}`)
+  return `Wake ${dogName} the ${DogBreed}`
 }
 
 function leashDog(dogName, DogBreed) {
   console.log(`Leash ${dogName} the ${DogBreed}`)
+  return `Leash ${dogName} the ${DogBreed}`
 }
 
 function walkToPark(dogName, DogBreed) {
   console.log(`Walk to the park with ${dogName} the ${DogBreed}`)
+  return `Walk to the park with ${dogName} the ${DogBreed}`
 }
 
 function throwFrisbee(dogName, DogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${DogBreed}`)
+  return `Throw the frisbee for ${dogName} the ${DogBreed}`
 }
 
 function walkHome(dogName, DogBreed) {
   console.log(`Walk home with ${dogName} the ${DogBreed}`)
+  return `Walk home with ${dogName} the ${DogBreed}`
 }
 
 function unleashDog(dogName, DogBreed) {
   console.log(`Unleash ${dogName} the ${DogBreed}`)
+  return `Unleash ${dogName} the ${DogBreed}`
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
