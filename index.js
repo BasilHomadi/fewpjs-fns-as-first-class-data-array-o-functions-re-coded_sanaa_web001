84695fd1c6723cc1d8640e3e280562aab2059c80
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 10:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function unleashDog(dogName, DogBreed) {
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
-function exerciseDog(dogName, dogBreed) {
-  return routine.map(fn => fn(dogName, dogBreed))
+function exerciseDog(dog, breed) {
+  return routine.map(fn => fn(dog, breed))
 }
